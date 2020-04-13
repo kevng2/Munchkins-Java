af04e488e08ba88bae4899dcb363d798edc4d416
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-04-13 18:11:11 -0400

    first commit

---
+++ Board.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Board extends JFrame {
+    private JPanel mMunchkinsJPanel;
+
+    private
+
+    Board() {
+        mMunchkinsJPanel = new JPanel();
+
+    }
+}
---
+++ Munchkins.java
@@ -0,0 +1,7 @@
+import javax.swing.JFrame;
+
+public class Munchkins {
+    public static void main(String[] args) {
+        Board game = new Board();
+    }
+}
